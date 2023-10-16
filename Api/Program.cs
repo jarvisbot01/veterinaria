3edafe4d599c09066e9d439610ec85ba858dc3b9
jarvisbot01 <jdrodriguez653@soy.sena.edu.co>
2023-10-16 13:31:57 +0000

    :sparkles: feat: configure database connection in Api and add initial migration
    - Configure database connection in Api.csproj, Program.cs, and appsettings.json.
    - Add initial database migration (20231016132438_InitialCreate) for PetShopContext in Persistence.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,6 @@
 using Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
 builder.Services.AddJwt(builder.Configuration);
+builder.Services.AddDbContext<PetShopContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
@@ -14,6 +21,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<PetShopContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var _logger = loggerFactory.CreateLogger<Program>();
+        _logger.LogError(ex, "An error occurred during migration.");
+    }
 }
 
 app.UseAuthorization();
